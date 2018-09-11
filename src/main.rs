e37484c3e6b062d8667f53961237919433a002c7
Shane Osbourne <shane.osbourne8@gmail.com>
2018-09-11 21:36:03 +0100

    cow example in regex

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-#![allow(unused_must_use)]
+extern crate regex;
 
-mod par;
-mod archy;
+use regex::Regex;
+
+use std::borrow::Cow;
+
 
 fn main() {
-    par::exec();
+    let subject = "abc";
+
+    let fns: Vec<fn(Cow<str>) -> Cow<str>> = vec![replace_a, replace_b];
+
+    let folded = fns.iter()
+        .fold(Cow::Owned(String::from(subject)), |acc, fn_item| fn_item(acc));
+
+    assert_eq!(folded, "ABc")
+}
+
+fn replace_a(input: Cow<str>) -> Cow<str> {
+    match input {
+        Cow::Borrowed(b) => Regex::new("a").unwrap().replace_all(b, "A"),
+        Cow::Owned(o) => Cow::Owned(Regex::new("a").unwrap().replace_all(&o, "A").into_owned()),
+    }
+}
+
+fn replace_b(input: Cow<str>) -> Cow<str> {
+    match input {
+        Cow::Borrowed(b) => Regex::new("b").unwrap().replace_all(b, "B"),
+        Cow::Owned(o) => Cow::Owned(Regex::new("b").unwrap().replace_all(&o, "B").into_owned()),
+    }
 }
