c3fc562c238bdf529423de207d94b08e38b1b355
Shane Osbourne <shane.osbourne8@gmail.com>
2018-09-11 22:20:30 +0100

    use a trait, and comment for learning

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,88 @@
 extern crate regex;
-
 use regex::Regex;
 
-use std::borrow::Cow;
+///
+/// A generic updater, it takes self, an options struct
+/// and a collection of things to apply.
+///
+/// In my case I'll be using fn's, but this definition
+/// does not enforce that.
+///
+trait Updater {
+    type Item;
+    type Options;
+    fn apply(self, opts: &Self::Item, items: Vec<Self::Item>) -> String;
+}
 
+///
+/// Just a struct for holding any options that might be
+/// needed in the 'apply' method.
+///
+struct MyOpts {
+    target: String,
+    next: String
+}
+
+///
+/// A simple wrapper struct for a String.
+/// This will be the 'subject'
+///
+struct Subject(String);
+
+///
+/// Now we can implement the Updater trait
+/// for our subject. We need to define the associated
+/// types Item & Options as declared in the Updater trait.
+///
+/// This is how we create generic functionality.
+///
+impl Updater for Subject {
+
+    // I want each Self::Item to be a fn of a certain signature
+    type Item = fn(&str, &MyOpts) -> String;
+
+    // And the options is just MyOpts
+    type Options = MyOpts;
+
+    ///
+    /// Now I can implement the apply method, in my case a fold
+    /// passing the original string value (self.0) through every fn in
+    /// sequence producing a single string output.
+    ///
+    fn apply(self, opts: &Self::Options, fns: Vec<Self::Item>) -> String {
+        fns.iter().fold(String::from(self.0), |acc, fn_item| fn_item(&acc, &opts))
+    }
+}
 
 fn main() {
-    let subject = "abc";
 
-    let fns: Vec<fn(Cow<str>) -> Cow<str>> = vec![replace_a, replace_b];
+    // simple options
+    let opts = MyOpts {
+        target: String::from("acme.de"),
+        next: String::from("127.0.0.1")
+    };
 
-    let folded = fns.iter()
-        .fold(Cow::Owned(String::from(subject)), |acc, fn_item| fn_item(acc));
+    // Create the struct holding a string, and pass along
+    // the options and the Vec of fns
+    let actual = Subject(String::from("https://acme.de"))
+        .apply(&opts, vec![
+            replace_host,
+            replace_scheme,
+        ]);
 
-    assert_eq!(folded, "ABc")
+    assert_eq!(actual, "//127.0.0.1")
 }
 
-fn replace_a(input: Cow<str>) -> Cow<str> {
-    match input {
-        Cow::Borrowed(b) => Regex::new("a").unwrap().replace_all(b, "A"),
-        Cow::Owned(o) => Cow::Owned(Regex::new("a").unwrap().replace_all(&o, "A").into_owned()),
-    }
+///
+/// Now we can create as many of these small, isolated fns
+/// that are easy to understand, easy to re-use and
+/// easy to test and we can dynamically choose which ones
+/// are applied at run time.
+///
+fn replace_host(input: &str, opts: &MyOpts) -> String {
+    Regex::new(&opts.target).unwrap().replace_all(&input, opts.next.as_str()).to_string()
 }
 
-fn replace_b(input: Cow<str>) -> Cow<str> {
-    match input {
-        Cow::Borrowed(b) => Regex::new("b").unwrap().replace_all(b, "B"),
-        Cow::Owned(o) => Cow::Owned(Regex::new("b").unwrap().replace_all(&o, "B").into_owned()),
-    }
+fn replace_scheme(input: &str, opts: &MyOpts) -> String {
+    Regex::new("https://").unwrap().replace_all(&input, "//").to_string()
 }
