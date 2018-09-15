7a062b508475cdb0f8795027f0c8a8ad17e08d9a
Shane Osbourne <shane.osbourne8@gmail.com>
2018-09-15 19:20:44 +0100

    more complicated examples for trait objects

--- src/trait_obj.rs
+++ src/trait_obj.rs
@@ -1,5 +1,5 @@
 type MwResult = Result<(), String>;
-trait Middleware {
+trait Middleware<T> {
     fn before(&self) -> MwResult {
         Ok(())
     }
@@ -8,21 +8,29 @@ trait Middleware {
     }
 }
 
-struct MyMiddleware;
+struct MyMiddleware1;
+struct MyMiddleware2;
+struct AppState;
 
-impl Middleware for MyMiddleware {
+impl Middleware<AppState> for MyMiddleware1 {
     fn before(&self) -> MwResult {
         Err(String::from("Ooops!"))
     }
 }
 
-pub fn run () {
-    let v: Vec<&Middleware> = vec![
-        &MyMiddleware{}
-    ];
-
-    for m in v.iter() {
-        let output = m.before();
-        assert_eq!(output, Err(String::from("Ooops!")));
+impl Middleware<AppState> for MyMiddleware2 {
+    fn before(&self) -> MwResult {
+        Ok(())
     }
+}
+
+fn get_middlewares<'a>() -> Vec<&'a Middleware<AppState>> {
+    vec![&MyMiddleware1 {}, &MyMiddleware2 {}]
+}
+
+pub fn run() {
+    let mws = get_middlewares();
+    let output: Vec<MwResult> = mws.iter().map(|mw| mw.before()).collect();
+    assert_eq!(output[0], Err(String::from("Ooops!")));
+    assert_eq!(output[1], Ok(()));
 }
