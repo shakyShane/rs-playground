2578da24fd1f86a53d5fd093a59293a9b07934fa
Shane Osbourne <shane.osbourne8@gmail.com>
2018-09-18 09:40:02 +0100

    Added a simple example of matching pairs with a tuple

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,12 @@ mod dyn_arg;
 mod compose;
 mod trait_obj;
 mod serialize;
+mod matching_tuple;
 
 fn main() {
     dyn_arg::run();
     compose::run();
     trait_obj::run();
     serialize::run();
+    matching_tuple::run();
 }
---
+++ src/matching_tuple.rs
@@ -0,0 +1,23 @@
+pub fn run() {
+    let match1 = matcher(1, "120");
+    let match2 = matcher(1, "240");
+    let match3 = matcher(1, "360");
+    let match4 = matcher(2, "400");
+
+    assert_eq!(match1, 1);
+    assert_eq!(match2, 2);
+    assert_eq!(match3, 2);
+    assert_eq!(match4, 0);
+}
+
+///
+/// An example of matching values as pairs using a
+/// temporary tuple
+///
+fn matcher(first: usize, second: &str) -> usize {
+    match (first, second) {
+        (1, "120") => 1,
+        (1, "240") | (1, "360") => 2,
+        (_first, _second) => 0
+    }
+}
